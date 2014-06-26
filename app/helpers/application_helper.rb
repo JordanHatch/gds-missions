97c79045341286edef5d47f957e87a930d5d118e
Jordan Hatch <jordan@jordanh.net>
2014-06-26 08:35:35 +0100

    Permit the <title> to be set without the header
    The ‘head_title’ block will just set the <title> tag, without adding
    a heading to the page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def page_title
+    title_parts = ["GDS Missions"]
+
+    if content_for?(:title)
+      title_parts << content_for(:title)
+    elsif content_for(:head_title)
+      title_parts << content_for(:head_title)
+    end
+
+    title_parts.reverse.join(" - ")
+  end
 end
